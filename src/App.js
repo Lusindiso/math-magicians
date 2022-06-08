333af395656d8d483acf31e9b80d006e84baa750
lUCI <lusindisontanjana@gmail.com>
2022-06-08 12:40:16 +0200

    Add multiple pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import Calculator from './components/Calculator';
+import {
+  Routes,
+  Route,
+} from 'react-router-dom';
+
+import CalculatorPage from './components/pages/CalculatorPage';
+import Home from './components/pages/Home';
+import Quote from './components/pages/Quote';
+import Header from './UI/Header';
 
 function App() {
-  return <Calculator />;
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/quote" element={<Quote />} />
+        <Route path="/calculator" element={<CalculatorPage />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
 }
-
 export default App;
